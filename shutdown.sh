907d5d6230c7dbc9ce9bee3b1a91a2869d59f048
John Allberg <john@ayoy.se>
2021-10-27 09:42:17 +0200

    Allow 5 secs of time before shutdown to not get error messages in ansible.

--- shutdown.sh
+++ shutdown.sh
@@ -1,2 +1,6 @@
-ansible -i hosts masters --become  -a "/usr/sbin/shutdown now"
-ansible -i hosts nodes --become  -a "/usr/sbin/shutdown now"
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ansible -i "${SCRIPT_DIR}/hosts" masters --become  -a "/usr/sbin/shutdown 5"
+ansible -i "${SCRIPT_DIR}/hosts" nodes   --become  -a "/usr/sbin/shutdown 5"
