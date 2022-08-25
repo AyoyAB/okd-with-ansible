bc5cd9795da48c358879491d6590da2e5fa98d4f
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2022-08-25 11:37:37 +0200

    Rename group nodes to workers (#39)

--- shutdown.sh
+++ shutdown.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ansible -i "${SCRIPT_DIR}/hosts" masters -m raw -a "/usr/sbin/shutdown 5" --become
-ansible -i "${SCRIPT_DIR}/hosts" nodes   -m raw -a "/usr/sbin/shutdown 5" --become
+ansible -i "${SCRIPT_DIR}/hosts" workers -m raw -a "/usr/sbin/shutdown 5" --become
