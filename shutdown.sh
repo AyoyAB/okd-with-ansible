f81af444b11cd2b544f82f7f1a398055c3742fda
John Allberg <john@ayoy.se>
2022-10-23 21:20:53 +0200

    Shutdown: Fix shutdown script after moving the hosts file.

--- shutdown.sh
+++ shutdown.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-ansible -i "${SCRIPT_DIR}/hosts" masters -m raw -a "/usr/sbin/shutdown 5" --become
-ansible -i "${SCRIPT_DIR}/hosts" workers -m raw -a "/usr/sbin/shutdown 5" --become
+ansible -i "${SCRIPT_DIR}/inventories/example/hosts" masters -m raw -a "/usr/sbin/shutdown 5" --become
+ansible -i "${SCRIPT_DIR}/inventories/example/hosts" workers -m raw -a "/usr/sbin/shutdown 5" --become
