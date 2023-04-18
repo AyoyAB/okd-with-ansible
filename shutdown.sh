0681bdf6e216b7d032323e7a4ff11b49a23dc5e8
John Allberg <john@ayoy.se>
2023-04-18 05:49:30 +0200

    Increase time between shutting down workers and masters.

--- shutdown.sh
+++ shutdown.sh
@@ -3,7 +3,7 @@
 # Therefore, for production clusters, set SECONDS_AFTER_WORKERS to 600.
 # https://docs.openshift.com/container-platform/4.8/backup_and_restore/graceful-cluster-shutdown.html
 MINUTES_TO_SHUTDOWN=${MINUTES_TO_SHUTDOWN:-0}
-SECONDS_AFTER_WORKERS=${SECONDS_AFTER_WORKERS:-90}
+SECONDS_AFTER_WORKERS=${SECONDS_AFTER_WORKERS:-120}
 
 certExpire=$(oc -n openshift-kube-apiserver-operator get secret kube-apiserver-to-kubelet-signer -o jsonpath='{.metadata.annotations.auth\.openshift\.io/certificate-not-after}')
 echo "Certificates will expire at ${certExpire}"
